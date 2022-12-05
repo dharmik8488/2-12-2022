7d0024ec18c16ff71fa1fa70823c71a3c161a213
dharmik8488 <118181437+dharmik8488@users.noreply.github.com>
2022-12-05 18:41:02 +0530

    Add files via upload

---
+++ 1.c
@@ -0,0 +1,41 @@
+#include<stdio.h>
+ 
+ 
+main(){
+ 	int i, j, a, b;
+  
+ 	printf("row line :-  ");
+ 	scanf("%i",&i);
+	
+	printf("colnmn line :-  ");
+ 	scanf("%i",&j);
+ 	
+	int c[i][j];
+
+ 	printf("Please Enter Elements\n");
+ 	
+	 for(a = 0; a < i; a++){
+   		
+		   for(b = 0; b < j; b++){
+		   	
+      		scanf("%d", &c[a][b]);
+    
+		}
+  
+  	}
+	
+	printf("Array\n");
+ 
+ 	for(a = 0; a < i; a++){
+ 		
+  		for(b = 0; b < j; b++){
+  			
+  			printf("%d  ", c[a][b]);
+	
+		}
+   	
+	   	printf("\n");
+  	
+	  }  	
+
+} 
